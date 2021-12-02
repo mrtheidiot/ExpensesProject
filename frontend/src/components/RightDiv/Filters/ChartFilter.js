6ab146b7174180beeb047462041b6a05eea20048
Piotr <mrtheidiot@gmail.com>
2021-12-02 01:11:58 +0100

    added yeear filter bar

--- frontend/src/components/RightDiv/Filters/ChartFilter.js
+++ frontend/src/components/RightDiv/Filters/ChartFilter.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import SearchByYear from "./SearchByYear";
 
 const ChartFilter = () => {
   const [expenses, setExpenses] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectFilter, setSelectFilter] = useState(1);
 
   const [selectedYear, setSelectedYear] = useState("2021");
   const [selectedCategory, setSelectedCategory] = useState();
@@ -33,58 +35,71 @@ const ChartFilter = () => {
   const selectedCategoryHandler = (event) => {
     setSelectedCategory(event.target.value);
   };
-  const selectedMonthHandler = event => {
-    setSelectedMonth(event.target.value)
-  }
+  const selectedMonthHandler = (event) => {
+    setSelectedMonth(event.target.value);
+  };
 
-  const searchByYearHandler = () => {};
+  const searchByYearHandler = () => {
+    setSelectFilter(1);
+  };
   const searchByCategoryHandler = () => {};
   const searchByCatAndMonthHandler = () => {};
 
   // <div></div>;
   return (
-    <form>
+    <div>
+      <form>
+        <div>
+          <label>Select Year</label>
+          <select onChange={selectedYearHandler} value={selectedYear}>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+          </select>
+          <button type="button" onClick={searchByYearHandler}>
+            Search by Year
+          </button>
+        </div>
+        <div>
+          <label>Select Category</label>
+          <select>
+            {categories.map((category, index) => {
+              return (
+                <option key={index} value={category.id}>
+                  {category.title}
+                </option>
+              );
+            })}
+          </select>
+          <button type="button" onClick={searchByCategoryHandler}>
+            Search by Category
+          </button>
+        </div>
+        <div>
+          <label>Search by Category in Month</label>
+          <select>
+            <option value="0">January</option>
+            <option value="1">february</option>
+            <option value="2">March</option>
+            <option value="3">April</option>
+            <option value="4">May</option>
+            <option value="5">June</option>
+            <option value="6">July</option>
+            <option value="7">August</option>
+            <option value="8">September</option>
+            <option value="9">October</option>
+            <option value="10">November</option>
+            <option value="11">December</option>
+          </select>
+          <button type="button" onClick={searchByCatAndMonthHandler}>
+            Search by Month and Category
+          </button>
+        </div>
+      </form>
       <div>
-        <label>Select Year</label>
-        <select onChange={selectedYearHandler} value={selectedYear}>
-          <option value="2023">2023</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-        </select>
-        <button type="button" onClick={searchByYearHandler}>
-          Search by Year
-        </button>
+        {selectFilter == 1 ? <SearchByYear expenses = {expenses} selected = {selectedYear}/> : null}
       </div>
-      <div>
-        <label>Select Category</label>
-        <select>
-          {categories.map((category, index) => {
-            return <option key={index} value={category.id}>{category.title}</option>;
-          })}
-        </select>
-        <button type="button" onClick={searchByCategoryHandler}>
-          Search by Category
-        </button>
-      </div>
-      <div>
-        <label>Search by Category in Month</label>
-        <select>
-          <option value="0">January</option>
-          <option value="1">february</option>
-          <option value="2">March</option>
-          <option value="3">April</option>
-          <option value="4">May</option>
-          <option value="5">June</option>
-          <option value="6">July</option>
-          <option value="7">August</option>
-          <option value="8">September</option>
-          <option value="9">October</option>
-          <option value="10">November</option>
-          <option value="11">December</option>
-        </select>
-        <button type="button" onClick={searchByCatAndMonthHandler}>Search by Month and Category</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
---
+++ frontend/src/components/RightDiv/Filters/SearchByYear.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Chart from "./Chart";
+
+const SearchByYear = (props) => {
+  const filteredExpenses = props.expenses.filter((expense) => {
+    const date = new Date(expense.created);
+    return date.getFullYear() == props.selected
+  });
+
+  const chartDataPoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+
+  for (const expense of filteredExpenses) {
+    const expneseMonth = new Date(expense.created).getMonth()
+    chartDataPoints[expneseMonth].value += +expense.price;
+  }
+
+
+  return (
+    <Chart dataPoints={chartDataPoints}/>
+  );
+};
+
+export default SearchByYear;
